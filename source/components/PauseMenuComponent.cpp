544b93be1b5b1038b09a40d59a18386676403ae6
Taha Rashid <taharashid710192@gmail.com>
2026-05-02 15:56:27 -0400

    Refactored PauseMenuComponent to use fn calls instead of if-else

--- source/components/PauseMenuComponent.cpp
+++ source/components/PauseMenuComponent.cpp
@@ -98,13 +98,20 @@ void PauseMenuComponent::update(int keys)
 
     if (keys & KEY_A)
     {
+        cancelSFX();
         sfxSelectHandle = musicCtrl.playSFX(SFX_SELECT, 255, 128);
+
         options[selectedOption].selected = true;
+        if (options[selectedOption].onSelect != nullptr) {
+            (this->*(options[selectedOption].onSelect))();
+        }
     }
 
     if (keys & KEY_B)
     {
-        sfxCancelHandle = musicCtrl.playSFX(SFX_CANCEL, 255, 128);
+        cancelSFX();
+        musicCtrl.playSFX(SFX_CANCEL, 255, 128);
+
         options[selectedOption].selected = false;
         selectedOption = 0;
 
@@ -117,245 +124,6 @@ void PauseMenuComponent::update(int keys)
     }
 
     consoleClear();
-
-    // MENU OPTIONS
-    if (menuOptions[SKILL].selected)
-    { // -> Skill options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[SKILL].selected = false;
-        selectedOption = 0;
-
-        options = skillOptions;
-        optionCount = SKILL_OPTIONS;
-    }
-    else if (menuOptions[ITEM].selected)
-    { // -> Item options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[ITEM].selected = false;
-        selectedOption = 0;
-
-        options = itemOptions;
-        optionCount = ITEM_OPTIONS;
-    }
-    else if (menuOptions[PERSONA].selected)
-    { // -> Persona options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[PERSONA].selected = false;
-        selectedOption = 0;
-
-        options = personaOptions;
-        optionCount = PERSONA_OPTIONS;
-    }
-    else if (menuOptions[EQUIP].selected)
-    { // -> Equip options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[EQUIP].selected = false;
-        selectedOption = 0;
-
-        options = equipOptions;
-        optionCount = EQUIP_OPTIONS;
-    }
-    else if (menuOptions[STATUS].selected)
-    { // -> Status options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[STATUS].selected = false;
-        selectedOption = 0;
-
-        options = statsOptions;
-        optionCount = STATS_OPTIONS;
-    }
-    else if (menuOptions[S_LINK].selected)
-    { // -> S.Link options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[S_LINK].selected = false;
-        selectedOption = 0;
-
-        options = sLinkOptions;
-        optionCount = S_LINK_OPTIONS;
-    }
-    else if (menuOptions[SYSTEM].selected)
-    { // -> System options
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-
-        menuOptions[SYSTEM].selected = false;
-        selectedOption = 0;
-
-        options = systemOptions;
-        optionCount = SYSTEM_OPTIONS;
-    }
-
-    // SKILL OPTIONS
-    else if (skillOptions[MAKOTO].selected)
-    { // "Makoto"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[YUKARI].selected)
-    { // "Yukari"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[JUNPEI].selected)
-    { // "Junpei"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[AKIHIKO].selected)
-    { // "Akihiko"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[MITSURU].selected)
-    { // "Mitsuru"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[AIGIS].selected)
-    { // "Aigis"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[KEN].selected)
-    { // "Ken"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[KOROMARU].selected)
-    { // "Koromaru"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (skillOptions[SHINJIRO].selected)
-    { // "Shinjiro"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-
-    // ITEM OPTIONS
-    else if (itemOptions[LIFE_STONE].selected)
-    { // "Life Stone"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (itemOptions[MEDICINE].selected)
-    { // "Medicine"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (itemOptions[BEAD].selected)
-    { // "Bead"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-
-    // EQUIP OPTINS
-    //...
-
-    // PERSONA OPTIONS
-    else if (personaOptions[JACK_FROST].selected)
-    { // "Jack Frost"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (personaOptions[BLACK_FROST].selected)
-    { // "Black Frost"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (personaOptions[KING_FROST].selected)
-    { // "King Frost"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-
-    // STATS OPTIONS
-    //...
-
-    // SLINK OPTIONS
-    else if (sLinkOptions[FOOL].selected)
-    { // "Fool"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (sLinkOptions[MAGICIAN].selected)
-    { // "Magician"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (sLinkOptions[EMPEROR].selected)
-    { // "Emperor"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    
-    // SYSTEM OPTIONS
-    else if (systemOptions[TUTORIAL].selected)
-    { // "Tutorial"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (systemOptions[CONFIG].selected)
-    { // "Config"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (systemOptions[DICTIONARY].selected)
-    { // "Dictionary"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (systemOptions[LOAD_DATA].selected)
-    { // "Load Data"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (systemOptions[SAVE_DATA].selected)
-    { // "Save Data"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
-    else if (systemOptions[RETURN_TO_TITLE].selected)
-    { // "Return to Title"
-        cancelSFX();
-        musicCtrl.playSFX(SFX_SELECT, 255, 128);
-        return;
-    }
 
     printf("\x1b[0;0H");
 
@@ -388,4 +156,117 @@ void PauseMenuComponent::update(int keys)
     }
 
     return;
+}
+
+// OPTION HANDLERS
+// menuOption handlers
+void PauseMenuComponent::openSkillMenu()
+{
+    menuOptions[SKILL].selected = false;
+    selectedOption = 0;
+
+    options = skillOptions;
+    optionCount = SKILL_OPTIONS;
+}
+
+void PauseMenuComponent::openItemMenu()
+{
+    menuOptions[ITEM].selected = false;
+    selectedOption = 0;
+
+    options = itemOptions;
+    optionCount = ITEM_OPTIONS;
+}
+
+void PauseMenuComponent::openPersonaMenu()
+{
+    menuOptions[PERSONA].selected = false;
+    selectedOption = 0;
+
+    options = personaOptions;
+    optionCount = PERSONA_OPTIONS;
+}
+
+void PauseMenuComponent::openEquipMenu()
+{
+    cancelSFX();
+    musicCtrl.playSFX(SFX_SELECT, 255, 128);
+
+    menuOptions[EQUIP].selected = false;
+    selectedOption = 0;
+
+    options = equipOptions;
+    optionCount = EQUIP_OPTIONS;
+}
+
+void PauseMenuComponent::openStatusMenu()
+{
+    cancelSFX();
+    musicCtrl.playSFX(SFX_SELECT, 255, 128);
+
+    menuOptions[STATUS].selected = false;
+    selectedOption = 0;
+
+    options = statsOptions;
+    optionCount = STATS_OPTIONS;
+}
+
+void PauseMenuComponent::openSLinkMenu()
+{
+    cancelSFX();
+    musicCtrl.playSFX(SFX_SELECT, 255, 128);
+
+    menuOptions[S_LINK].selected = false;
+    selectedOption = 0;
+
+    options = sLinkOptions;
+    optionCount = S_LINK_OPTIONS;
+}
+
+void PauseMenuComponent::openSystemMenu()
+{
+    cancelSFX();
+    musicCtrl.playSFX(SFX_SELECT, 255, 128);
+
+    menuOptions[SYSTEM].selected = false;
+    selectedOption = 0;
+
+    options = systemOptions;
+    optionCount = SYSTEM_OPTIONS;
+}
+
+// generic handlers
+void PauseMenuComponent::skillOptionSelected()
+{
+    // ...
+}
+
+void PauseMenuComponent::itemOptionSelected()
+{
+    // ...
+}
+
+void PauseMenuComponent::equipOptionSelected()
+{
+    // ...
+}
+
+void PauseMenuComponent::personaOptionSelected()
+{
+    // ...
+}
+
+void PauseMenuComponent::statsOptionSelected()
+{
+    // ...
+}
+
+void PauseMenuComponent::sLinkOptionSelected()
+{
+    // ...
+}
+
+void PauseMenuComponent::systemOptionSelected()
+{
+    // ...
 }
--- source/components/PauseMenuComponent.h
+++ source/components/PauseMenuComponent.h
@@ -9,11 +9,14 @@
 #define STATS_OPTIONS 9
 #define S_LINK_OPTIONS 3
 #define SYSTEM_OPTIONS 6
+
+class PauseMenuComponent;
 typedef struct
 {
     const char* name;
     bool selected;
     int bgIndex = -1;
+    void (PauseMenuComponent::*onSelect)();
 } PauseOption;
 
 // Menu options
@@ -94,92 +97,111 @@ class PauseMenuComponent {
 
         PauseOption menuOptions[MENU_OPTIONS] = 
         {
-            {"Skill", false},
-            {"Item", false},
-            {"Persona", false},
-            {"Equip", false},
-            {"Status", false},
-            {"S.Link", false},
-            {"System", false},
+            {"Skill", false, -1, &PauseMenuComponent::openSkillMenu},
+            {"Item", false, -1, &PauseMenuComponent::openItemMenu},
+            {"Persona", false, -1, &PauseMenuComponent::openPersonaMenu},
+            {"Equip", false, -1, &PauseMenuComponent::openEquipMenu},
+            {"Status", false, -1, &PauseMenuComponent::openStatusMenu},
+            {"S.Link", false, -1, &PauseMenuComponent::openSLinkMenu},
+            {"System", false, -1, &PauseMenuComponent::openSystemMenu},
         };
 
         // TODO: go into submenus
         PauseOption skillOptions[SKILL_OPTIONS] = 
         {
-            {"Makoto", false, 0},
-            {"Yukari", false, 1},
-            {"Junpei", false, 3},
-            {"Akihiko", false, 2},
-            {"Mitsuru", false},
-            {"Aigis", false},
-            {"Ken", false},
-            {"Koromaru", false},
-            {"Shinjiro", false},
+            {"Makoto", false, 0, &PauseMenuComponent::skillOptionSelected},
+            {"Yukari", false, 1, &PauseMenuComponent::skillOptionSelected},
+            {"Junpei", false, 3, &PauseMenuComponent::skillOptionSelected},
+            {"Akihiko", false, 2, &PauseMenuComponent::skillOptionSelected},
+            {"Mitsuru", false, -1, &PauseMenuComponent::skillOptionSelected},
+            {"Aigis", false, -1, &PauseMenuComponent::skillOptionSelected},
+            {"Ken", false, -1, &PauseMenuComponent::skillOptionSelected},
+            {"Koromaru", false, -1, &PauseMenuComponent::skillOptionSelected},
+            {"Shinjiro", false, -1, &PauseMenuComponent::skillOptionSelected},
         };
 
         PauseOption itemOptions[ITEM_OPTIONS] = 
         {
-            {"Life Stone", false},
-            {"Medicine", false},
-            {"Bead", false},
+            {"Life Stone", false, -1, &PauseMenuComponent::itemOptionSelected},
+            {"Medicine", false, -1, &PauseMenuComponent::itemOptionSelected},
+            {"Bead", false, -1, &PauseMenuComponent::itemOptionSelected},
         };
 
         // TODO: go into submenus
         PauseOption equipOptions[EQUIP_OPTIONS] = 
         {
-            {"Makoto", false},
-            {"Yukari", false},
-            {"Junpei", false},
-            {"Akihiko", false},
-            {"Mitsuru", false},
-            {"Aigis", false},
-            {"Ken", false},
-            {"Koromaru", false},
-            {"Shinjiro", false},
+            {"Makoto", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Yukari", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Junpei", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Akihiko", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Mitsuru", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Aigis", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Ken", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Koromaru", false, -1, &PauseMenuComponent::equipOptionSelected},
+            {"Shinjiro", false, -1, &PauseMenuComponent::equipOptionSelected},
         };
 
         PauseOption personaOptions[PERSONA_OPTIONS] = 
         {
-            {"Jack Frot", false},
-            {"Black Frost", false},
-            {"King Frost", false},
+            {"Jack Frot", false, -1, &PauseMenuComponent::personaOptionSelected},
+            {"Black Frost", false, -1, &PauseMenuComponent::personaOptionSelected},
+            {"King Frost", false, -1, &PauseMenuComponent::personaOptionSelected},
         };
 
         // TODO: go into submenus
         PauseOption statsOptions[STATS_OPTIONS] = 
         {
-            {"Makoto", false},
-            {"Yukari", false},
-            {"Junpei", false},
-            {"Akihiko", false},
-            {"Mitsuru", false},
-            {"Aigis", false},
-            {"Ken", false},
-            {"Koromaru", false},
-            {"Shinjiro", false},
+            {"Makoto", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Yukari", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Junpei", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Akihiko", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Mitsuru", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Aigis", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Ken", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Koromaru", false, -1, &PauseMenuComponent::statsOptionSelected},
+            {"Shinjiro", false, -1, &PauseMenuComponent::statsOptionSelected},
         };
 
         // TODO: go into submenus
         PauseOption sLinkOptions[S_LINK_OPTIONS] =
         {
-            {"Fool", false},
-            {"Magician", false},
-            {"Emperor", false},
+            {"Fool", false, -1, &PauseMenuComponent::sLinkOptionSelected},
+            {"Magician", false, -1, &PauseMenuComponent::sLinkOptionSelected},
+            {"Emperor", false, -1, &PauseMenuComponent::sLinkOptionSelected},
         };
 
         // TODO: go into submenus
         PauseOption systemOptions[SYSTEM_OPTIONS] = 
         {
-            {"Tutorial", false},
-            {"Config", false},
-            {"Dictionary", false},
-            {"Load Data", false},
-            {"Save Data", false},
-            {"Return to Title", false},
+            {"Tutorial", false, -1, &PauseMenuComponent::systemOptionSelected},
+            {"Config", false, -1, &PauseMenuComponent::systemOptionSelected},
+            {"Dictionary", false, -1, &PauseMenuComponent::systemOptionSelected},
+            {"Load Data", false, -1, &PauseMenuComponent::systemOptionSelected},
+            {"Save Data", false, -1, &PauseMenuComponent::systemOptionSelected},
+            {"Return to Title", false, -1, &PauseMenuComponent::systemOptionSelected},
         };
 
         void setBgLoaders();
         void loadBg(int bgIndex);
+
+        // OPTION HANDLERS
+        // menuOption handlers
+        void openSkillMenu();
+        void openItemMenu();
+        void openPersonaMenu();
+        void openEquipMenu();
+        void openStatusMenu();
+        void openSLinkMenu();
+        void openSystemMenu();
+        // generic handlers
+        void skillOptionSelected();
+        void itemOptionSelected();
+        void equipOptionSelected();
+        void personaOptionSelected();
+        void statsOptionSelected();
+        void sLinkOptionSelected();
+        void systemOptionSelected();
+
     public:
         void init(int iBgSlot);
         void update(int keys);
